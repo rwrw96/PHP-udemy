9947da3c598e3603e61674b20efbf1bf7963cfc5
rwrw96 <kazuki137@gamil.com>
2021-08-01 14:33:37 +0900

    返信機能を追加

--- mini_app/index.php
+++ mini_app/index.php
@@ -16,10 +16,11 @@ if(isset($_SESSION['id']) && $_SESSION['time'] + 3600 > time()){
 if(!empty($_POST)) {
   if($_POST['message'] !== ''){
 
-    $message = $db -> prepare('INSERT INTO posts SET member_id=?, message=?, created=NOW()');
+    $message = $db -> prepare('INSERT INTO posts SET member_id=?, message=?, repry_message_id=?, created=NOW()');
     $message -> execute(array(
       $member['id'],
-      $_POST['message']
+      $_POST['message'],
+      $_POST['reply_post_id']
     ));
     header('Location: index.php');
   }
@@ -27,6 +28,15 @@ if(!empty($_POST)) {
 
 $posts = $db -> query('SELECT members.name, members.picture, posts.* FROM members, posts
                       WHERE members.id = posts.member_id ORDER BY posts.created DESC');
+
+if(isset($_REQUEST['res'])){
+  $response = $db -> prepare('SELECT members.name, members.picture, posts.* FROM members, posts
+  WHERE members.id = posts.member_id AND posts.id=?');
+  $response -> execute(array($_REQUEST['res'])); 
+  $table = $response -> fetch();
+
+  $message = '@' . $table['name'] . ' ' . $table['message'];  
+}
 ?>
 
 <!DOCTYPE html>
@@ -51,9 +61,10 @@ $posts = $db -> query('SELECT members.name, members.picture, posts.* FROM member
       <dl>
         <dt><?PHP print(htmlspecialchars($member['name'], ENT_QUOTES)); ?>さん、メッセージをどうぞ</dt>
         <?php print($_POST['message']); ?>
+        <?php var_dump($_REQUEST['res']); ?>
         <dd>
-          <textarea name="message" cols="50" rows="5"></textarea>
-          <input type="hidden" name="reply_post_id" value="" />
+          <textarea name="message" cols="50" rows="5"><?php print($message); ?></textarea>
+          <input type="hidden" name="reply_post_id" value="<?php print($_REQUEST['res']); ?>" />
         </dd>
       </dl>
       <div>
@@ -66,10 +77,9 @@ $posts = $db -> query('SELECT members.name, members.picture, posts.* FROM member
     <?php foreach($posts as $post): ?>
     <div class="msg">
     <img src="member_picture/<?php print($member['picture']); ?>" width="48" height="48" alt="<?php print($post['name']); ?>" />
-    <p><?php print($post['message']); ?><span class="name">（<?php print($post['name']); ?>）</span>[<a href="index.php?res=">Re</a>]</p>
+    <p><?php print($post['message']); ?><span class="name">（<?php print($post['name']); ?>）</span>[<a href="index.php?res=<?php print($post['id']); ?>">Re</a>]</p>
     <p class="day"><?php print($post['created']); ?><a href="view.php?id="></a>
-<a href="view.php?id=">
-返信元のメッセージ</a>
+<a href="view.php?id=">返信元のメッセージ</a>
 [<a href="delete.php?id="
 style="color: #F33;">削除</a>]
     </p>
